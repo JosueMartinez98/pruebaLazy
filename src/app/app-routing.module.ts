cf56c5ee3d3a97dd79496b066f8ae90c545dceb8
josue ortega <josueortegamtz98@gmail.com>
2020-12-30 17:03:43 -0600

    aplicando lazy loading y mod shared creado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then(m => m.HomeModule)
+  },
+  {
+    path: 'contacto',
+    loadChildren: () => import('./contacto/contacto.module').then(m => m.ContactoModule)
+  },
+  {
+    path: 'productos',
+    loadChildren: () => import('./productos/productos.module').then(m => m.ProductosModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
